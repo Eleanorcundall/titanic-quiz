537a2f95ac76727a17b4ca65f53a80cc9cfb2f6a
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-08-06 10:26:16 +0100

    Add 12 more questions to questions variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let questions = [
   {
-    question: "1?",
+    question: "What was the Titanics official name?",
     choices: ["Royal Mail Ship", "Royal Marine Ship", "Racing Marine Steamer"],
     answer: "0",
   },
@@ -14,8 +14,92 @@ let questions = [
     answer: "0",
   },
   {
-    question: "3?",
-    choices: ["Royal Mail Ship", "Royal Marine Ship", "Racing Marine Steamer"],
+    question: "What were the Titanics sister ships? ",
+    choices: [
+      "RMS Empress of China and RMS Empress of India ",
+      "RMS St Columba and RMS Llewellyn",
+      "RMS Olympic and HMHS Britannic",
+    ],
+    answer: "2",
+  },
+  {
+    question: "The Titanic was en route to what city when it sunk? ",
+    choices: [
+      "Jersey City, New Jersey, United States ",
+      "New York City, United States ",
+      "Morehead City, North Carolina, United States",
+    ],
+    answer: "1",
+  },
+  {
+    question: "Who was the captain of the Titanic? ",
+    choices: [
+      "Captain Edward ‘Blackbeard’ Teach",
+      "Captain Edward John Smith",
+      "Captain Archibald Haddock",
+    ],
+    answer: "1",
+  },
+  {
+    question: "The Titanic had a crew of about how many?",
+    choices: ["About 100", "About 450", "About 900"],
+    answer: "2",
+  },
+  {
+    question: "What did the Titanic hit, which caused it to sink?",
+    choices: ["A rock", "An iceberg", "Another ship"],
+    answer: "1",
+  },
+  {
+    question: "How many people were aboard the Titanic when it sunk?",
+    choices: ["About 500", "About 1000", "About 2200"],
+    answer: "2",
+  },
+  {
+    question: "How many dogs were aboard the Titanic?",
+    choices: ["3", "12", "6"],
+    answer: "1",
+  },
+  {
+    question: "What was the date that it sank?",
+    choices: [
+      "1 – 2 August, 1952 ",
+      "14 – 15 April, 1912 ",
+      "24 – 25 December, 1972 ",
+    ],
+    answer: "1",
+  },
+  {
+    question:
+      "The Titanic was equipped to hold 64 lifeboats, but how many did it actually have?",
+    choices: ["20", "65", "63"],
+    answer: "0",
+  },
+  {
+    question:
+      "Each lifeboat could hold 40 to 65 people (depending on what style it was). How many people were aboard the first lifeboat? ",
+    choices: ["28", "40", "65"],
+    answer: "0",
+  },
+  {
+    question:
+      "What was the name of the ship that came to the Titanic’s rescue?",
+    choices: ["RMS Empress of China", "RMS Empress of India ", "RMS Carpathia"],
+    answer: "2",
+  },
+  {
+    question:
+      " At least how many people lost their lives when the Titanic sunk",
+    choices: ["At least 100 ", "At least 500", "At least 1,500"],
+    answer: "2",
+  },
+  {
+    question: "How many dogs survived the sinking?",
+    choices: [
+      "3",
+      "6",
+      "12",
+    ],
     answer: "2",
   },
 ];
