80a744a3c4dacb6162bd151302130f218d856640
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-08-06 09:56:27 +0100

    Add main HTML stucture in place, some basic CSS and add dummy questions to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let questions = [
+  {
+    question: "1?",
+    choices: ["Royal Mail Ship", "Royal Marine Ship", "Racing Marine Steamer"],
+    answer: "0",
+  },
+  {
+    question: "Where was the Titanic built? ",
+    choices: [
+      "In the Harland and Wolff Shipyard, Belfast, Northern Ireland ",
+      "In the Suez Shipyard, Egypt",
+      " In the Western Marine Shipyard, Bangladesh",
+    ],
+    answer: "0",
+  },
+  {
+    question: "3?",
+    choices: ["Royal Mail Ship", "Royal Marine Ship", "Racing Marine Steamer"],
+    answer: "2",
+  },
+];
