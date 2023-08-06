a978145621676f30c85acb0092783621a237ad50
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-08-06 09:58:22 +0100

    Create show question function, add DOMloaded event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,35 @@ let questions = [
     answer: "2",
   },
 ];
+let currentQuestionIndex = 0;
+
+document.addEventListener("DOMContentLoaded", showQuestion);
+
+let submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", checkAnswer);
+
+function showQuestion() {
+  let questionContainer = document.getElementById("displayQuestions");
+  questionContainer.innerHTML = ""; // Clear existing question
+
+  let questionText = document.createElement("p");
+  questionText.textContent = questions[currentQuestionIndex].question;
+  questionContainer.appendChild(questionText);
+
+  let answerChoices = questions[currentQuestionIndex].choices;
+  for (let i = 0; i < answerChoices.length; i++) {
+    let choiceContainer = document.createElement("div");
+
+    let choiceText = document.createElement("p");
+    choiceText.textContent = answerChoices[i];
+    choiceContainer.appendChild(choiceText);
+
+    let choiceInput = document.createElement("input");
+    choiceInput.type = "radio";
+    choiceInput.name = "choices";
+    choiceInput.value = i;
+
+    choiceContainer.appendChild(choiceInput);
+    questionContainer.appendChild(choiceContainer);
+  }
+}
